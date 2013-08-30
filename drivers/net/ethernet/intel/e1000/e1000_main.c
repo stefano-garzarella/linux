bbc75c8b06b576ba529d0f1b8e4d9663e7c8942d
Vincenzo Maffione <v.maffione@gmail.com>
2013-08-30 14:36:03 +0200

    e1000: tcp partial checksum computation
    In paravirtual mode, with TSO packets, the driver used to compute
    a partial checksum in e1000_tso(), using the csum_tcpudp_magic()
    function. Once again (see the last commit), this is not necessary since
    we are using virtio-net header, and so we can hand off an unchecksummed
    packet (that won't be segmented) to the emulator.
    This saves CPU cycles, incrementing the TCP throughput.

--- drivers/net/ethernet/intel/e1000/e1000_main.c
+++ drivers/net/ethernet/intel/e1000/e1000_main.c
@@ -2894,13 +2894,13 @@ static int e1000_tso(struct e1000_adapter *adapter,
 		if (skb->protocol == htons(ETH_P_IP)) {
 			struct iphdr *iph = ip_hdr(skb);
 			if (!adapter->csb_mode) {
-			    iph->tot_len = 0;
-			    iph->check = 0;
-                        }
-			tcp_hdr(skb)->check = ~csum_tcpudp_magic(iph->saddr,
+			        iph->tot_len = 0;
+			        iph->check = 0;
+			        tcp_hdr(skb)->check = ~csum_tcpudp_magic(iph->saddr,
 								 iph->daddr, 0,
 								 IPPROTO_TCP,
 								 0);
+                        }
 			cmd_length = E1000_TXD_CMD_IP;
 			ipcse = skb_transport_offset(skb) - 1;
 		} else if (skb->protocol == htons(ETH_P_IPV6)) {
