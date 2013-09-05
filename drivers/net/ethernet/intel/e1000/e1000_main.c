6c36b1bc2228ef19e884bc5c57f3a292daf05c51
Vincenzo Maffione <v.maffione@gmail.com>
2013-09-05 14:51:49 +0200

    e1000: move back tcpudp_magic out of if() { }
    This is necessary for now, so that TSO works (very slowly, but
    works) with QEMU backends that do not support the virtio-net
    header (we have decided to require the driver to ALWAYS use
    the header in paravirtual mode).
    In the future also the tcpudp_magic() operation will be done
    by the QEMU frontend, so that we can move again this inside
    the if construct.

--- drivers/net/ethernet/intel/e1000/e1000_main.c
+++ drivers/net/ethernet/intel/e1000/e1000_main.c
@@ -2896,11 +2896,13 @@ static int e1000_tso(struct e1000_adapter *adapter,
 			if (!adapter->csb_mode) {
 			        iph->tot_len = 0;
 			        iph->check = 0;
+                        }
+                        /* TODO do csum_tcpudp_magic() into the frontend only when not using vnet_hdr,
+                            and move back this line into the last if construct.*/
 			        tcp_hdr(skb)->check = ~csum_tcpudp_magic(iph->saddr,
 								 iph->daddr, 0,
 								 IPPROTO_TCP,
 								 0);
-                        }
 			cmd_length = E1000_TXD_CMD_IP;
 			ipcse = skb_transport_offset(skb) - 1;
 		} else if (skb->protocol == htons(ETH_P_IPV6)) {
@@ -3163,7 +3165,7 @@ static void e1000_tx_queue(struct e1000_adapter *adapter,
 			     E1000_TXD_CMD_TSE;
 		txd_upper |= E1000_TXD_POPTS_TXSM << 8;
 
-		if (!adapter->csb_mode && tx_flags & E1000_TX_FLAGS_IPV4)
+		if (tx_flags & E1000_TX_FLAGS_IPV4)
 			txd_upper |= E1000_TXD_POPTS_IXSM << 8;
 	}
 
